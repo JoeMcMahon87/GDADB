3136f90d6cda2ee35485c3c055650cd640b5b81b
Joe McMahon <mcmahon.joe@verizon.net>
2016-05-10 12:45:42 +0000

    Adding autocomplet JS file

---
+++ public/assets/js/autocomplete.js
@@ -0,0 +1,15 @@
+$(function() {
+    function log( message ) {
+      $( "<div>" ).text( message ).prependTo( "#log" );
+      $( "#log" ).scrollTop( 0 );
+    }
+
+$.ui.autocomplete.prototype._renderItem = function (ul, item) {
+    var re = new RegExp($.trim(this.term));
+    var t = item.label.replace(re, "<span style='font-weight:600;color:#5C5C5C;'>" + $.trim(this.term) +
+        "</span>");
+    return $("<li></li>")
+        .data("item.autocomplete", item)
+        .append("<a>" + t + "</a>")
+        .appendTo(ul);
+};
